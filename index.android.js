73e647dcd6dcc825ddfacfc2de2c410ba80cf04d
aloerina01 <kiss_mint27@yahoo.co.jp>
2017-05-29 01:37:36 +0900

    Implement Compoent sample page

--- index.android.js
+++ index.android.js
@@ -5,49 +5,52 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Container, Content, Button, Header, Body, Title, List, ListItem, 
+        Card, CardItem, Badge, Icon, Left, Right, Text, H3 } from 'native-base';
+import { AppRegistry } from 'react-native';
 
 export default class HelloReactNative extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Container>
+        <Header>
+            <Body>
+              <Title>Component Sample</Title>
+            </Body>
+        </Header>
+        <Content padder>
+          <H3>Button</H3>
+          <Button><Text>select</Text></Button>
+          <H3>Budge</H3>
+          <Badge><Text>2</Text></Badge>
+          <H3>Card</H3>
+          <Card>
+            <CardItem>
+                <Icon name="logo-twitter" style={{ color: '#55acee' }} />
+                <Text>Twitter</Text>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+            </CardItem>
+            <CardItem>
+                <Icon name="logo-googleplus" style={{ color: '#dd4b39' }} />
+                <Text>Google Plus</Text>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+            </CardItem>
+            <CardItem>
+              <Icon name="logo-facebook" style={{ color: '#315096' }} />
+              <Text>Facebook</Text>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('HelloReactNative', () => HelloReactNative);
